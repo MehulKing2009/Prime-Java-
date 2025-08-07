b61392607e38a070b8b84f643712bc35a35a1e1e
MehulKing2009 <151845785+MehulKing2009@users.noreply.github.com>
2025-08-07 18:55:58 +0530

    Prime.java

--- Prime.java
+++ Prime.java
@@ -4,13 +4,13 @@ class Prime
     {
      long x,y,z;
      System.out.println("2");
-     for(x=3L;;x=x+1) // add limit by for(x=3L;x<="Limit";x=x+1)
+     for(x=3L;;x=x+1) // add limit by for(x=3L;x<=342848;x=x+1)
      { z=0;
         for (y=2L;y<x;y=y+1)
         {
             if (x%y==0)
           {
-            z=1;
+            z=69; // Do not change to 0
           }
         }
         if (z==0)
