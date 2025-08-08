22b20743fb1210327192a1860ae9f95113906799
MehulKing2009 <151845785+MehulKing2009@users.noreply.github.com>
2025-08-08 12:05:54 +0530

    Prime.java

--- Prime.java
+++ Prime.java
@@ -4,7 +4,7 @@ class Prime
     {
      long x,y,z;
      System.out.println("2");
-     for(x=3L;;x=x+1) // add limit by for(x<=345L;x<=342848;x=x+1)
+     for(x=3L;;x=x+1) // add limit by for(x=3L;x<=3448;x=x+1)
      { z=0;
         for (y=2L;y<x;y=y+1)
         {
